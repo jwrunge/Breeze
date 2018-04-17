b4723d484a99a1bba49d358ea526dc636e70a06b
Jacob Runge <jwrunge@gmail.com>
2018-04-17 07:13:45 -0500

    Set up controller skeleton, test skeletons

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,29 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    /**
+     * I'm putting all my controller logic in the main controller because this is going to be a small app. Ordinarily, I would extend the Controller into multiple, modular controllers.
+     */
+
+    /**
+     * These functions are not meant to be called from a route; they are to be used only by other class methods
+     */
+    protected function add_people($people) {
+        //Add people, return report
+    }
+
+    protected function add_groups($groups) {
+        //Add group, return report
+    }
+
+    /**
+     * These are to be accessible via the request routes
+     */
+    public function handle_people_csv(Request $request) {
+        //Load a CSV file for people, pass to add_people
+    }
+
+    public function handle_groups_csv(Request $request) {
+        //Load a CSV file for groups, pass to add_groups
+    }
 }
---
+++ tests/PeopleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class PeopleTest extends TestCase
+{
+    use DatabaseTransactions; //Auto cleanup database changes
+
+    //Ensure multiple people additions occur even with modifications needed
+    public function test_add_and_modify_multiple_people()
+    {
+        //Supply people data
+
+        //Post to database
+
+        //Modify people data
+
+        //Post to database again
+
+        //Make sure both modified and added people are present
+        
+    }
+}
