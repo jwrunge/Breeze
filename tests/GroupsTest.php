619470cbec5832808baf4888859943ba195f928a
Jacob Runge <jwrunge@gmail.com>
2018-04-17 07:39:21 -0500

    Filled out test files -- I will write controllers to pass tests

--- tests/GroupsTest.php
+++ tests/GroupsTest.php
@@ -7,18 +7,44 @@ class GroupTest extends TestCase
 {
     use DatabaseTransactions; //Auto cleanup database changes
 
-    //Ensure multiple people additions occur even with modifications needed
-    public function test_add_and_modify_multiple_people()
+    //Ensure multiple group additions occur even with modifications needed
+    public function test_add_and_modify_multiple_groups()
     {
-        //Supply people data
+        //Supply group data
+        $peopledata = [
+            [
+                'group_id' => 1,
+                'group_name' => 'Bible Study'
+            ],
+            [
+                'group_id' => 3,
+                'group_name' => 'Sunday School'
+            ],
+            [
+                'group_id' => 5,
+                'group_name' => 'Worship'
+            ],
+        ];
 
         //Post to database
+        $this->json('POST', '/command/addgroupscsv', $peopledata);
 
-        //Modify people data
+        //Modify group data
+        $peopledata[0] = [ 
+            'group_id' => 1,
+            'group_name' => 'Spaghetti Dinner' 
+        ];
+        array_push($people_data, [
+            'group_id' => 10,
+            'group_name' => 'Outreach'
+        ]);
 
-        //Post to database again
+        //Post to database
+        $this->json('POST', '/command/addgroupscsv', $peopledata);
 
-        //Make sure both modified and added people are present
-        
+        //Make sure both modified and added groups are present (Group 1 should be modified, 3 should be the same, 10 should be new)
+        $this->seeInDatabase('groups', ['group_id' => 1, 'group_name' => 'Spaghetti Dinner']);
+        $this->seeInDatabase('groups', ['group_id' => 3, 'group_name' => 'Sunday School']);
+        $this->seeInDatabase('groups', ['group_id' => 10, 'group_name' => 'Outreach']);
     }
 }
--- tests/PeopleTest.php
+++ tests/PeopleTest.php
@@ -11,14 +11,51 @@ class PeopleTest extends TestCase
     public function test_add_and_modify_multiple_people()
     {
         //Supply people data
+        $peopledata = [
+            [
+                'person_id' => 1,
+                'first_name' => 'Jake',
+                'last_name' => 'Runge',
+                'email_address' => 'jwrunge@gmail.com',
+            ],
+            [
+                'person_id' => 3,
+                'first_name' => 'Mary',
+                'last_name' => 'Runge',
+                'email_address' => 'maryrunge@gmail.com',
+            ],
+            [
+                'person_id' => 5,
+                'first_name' => 'Lucky',
+                'last_name' => 'Runge',
+                'email_address' => 'luckyrunge@gmail.com',
+            ]
+        ];
 
         //Post to database
+        $this->json('POST', '/command/addpeoplecsv', $peopledata);
 
         //Modify people data
+        $peopledata[0] = [
+            'person_id' => 1,
+            'first_name' => 'NOT_Jake',
+            'last_name' => 'Runge',
+            'email_address' => 'jwrunge@gmail.com',
+        ];
+
+        array_push($people_data, [
+            'person_id' => 10,
+            'first_name' => 'Max',
+            'last_name' => 'Runge',
+            'email_address' => 'maxrunge@gmail.com',
+        ]);
 
         //Post to database again
+        $this->json('POST', '/command/addpeoplecsv', $peopledata);
 
-        //Make sure both modified and added people are present
-        
+        //Make sure both modified and added people are present (person 1 should be modified, 3 should be the same, 10 should be new)
+        $this->seeInDatabase('people', ['person_id' => 1, 'first_name' => 'NOT_Jake']);
+        $this->seeInDatabase('people', ['person_id' => 3, 'first_name' => 'Mary']);
+        $this->seeInDatabase('people', ['person_id' => 10, 'first_name' => 'Max']);
     }
 }
